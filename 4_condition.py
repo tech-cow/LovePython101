d720fe9531883a66972fc2485b4d690fcd57561e
Yu Zhou <yui@Yus-MacBook-Pro.local>
2017-09-17 12:36:36 -0700

    update

--- 4_condition.py
+++ 4_condition.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 # Comparisons:
 # Equal:            ==
@@ -8,10 +10,21 @@
 # Less or Equal:    <=
 # Object Identity:  is
 
+#上帝视角
+player = ['wolf','wolf','wolf','seer','witch','hunter','fart villager','fart villager','fart villager']
 
-# False Values:
-    # False
-    # None
-    # Zero of any numeric type
-    # Any empty sequence. For example, '', (), [].
-    # Any empty mapping. For example, {}.
+#狼人请睁眼
+temp = input("狼人请睁眼，请选择猎杀目标")
+
+#女巫请睁眼
+isSave = input('女巫请睁眼，救人请输入1，不救人请输入0')
+
+
+#上帝发言
+if isSave == True:
+    print '昨晚平安夜'
+else:
+    identity = player.pop(temp)
+    print '昨晚' + str(temp) + '玩家死亡, 他的身份是: ' + identity 
+
+print player
