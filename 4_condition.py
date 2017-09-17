4c2a23f4d2d541c8049991c83d364eae95444fa7
Yu Zhou <yui@Yus-MacBook-Pro.local>
2017-09-17 12:45:36 -0700

    update

--- 4_condition.py
+++ 4_condition.py
@@ -19,12 +19,31 @@ temp = input("狼人请睁眼，请选择猎杀目标")
 #女巫请睁眼
 isSave = input('女巫请睁眼，救人请输入1，不救人请输入0')
 
+#上帝发言1
+def annocement1():
+    if isSave == True:
+        print '昨晚平安夜'
+    else:
+        identity = player.pop(temp)
+        print '昨晚' + str(temp) + '玩家死亡, 他的身份是: ' + identity
+    print player
 
-#上帝发言
-if isSave == True:
-    print '昨晚平安夜'
-else:
-    identity = player.pop(temp)
-    print '昨晚' + str(temp) + '玩家死亡, 他的身份是: ' + identity 
+#上帝发言2
+def annocement2():
+    if isSave is True:
+        print '昨晚平安夜'
+    else:
+        identity = player.pop(temp)
+        print '昨晚' + str(temp) + '玩家死亡, 他的身份是: ' + identity
+    print player
 
-print player
+#上帝发言3
+def annocement3():
+    if isSave:
+        print '昨晚平安夜'
+    else:
+        identity = player.pop(temp)
+        print '昨晚' + str(temp) + '玩家死亡, 他的身份是: ' + identity
+    print player
+
+annocement3()
