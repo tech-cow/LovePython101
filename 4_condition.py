c794c8750acffe3ead9af1e92f6c05b6dea9dfd0
Yu Zhou <yui@Yus-MacBook-Pro.local>
2017-09-17 12:08:33 -0700

    .

---
+++ 4_condition.py
@@ -0,0 +1,17 @@
+
+# Comparisons:
+# Equal:            ==
+# Not Equal:        !=
+# Greater Than:     >
+# Less Than:        <
+# Greater or Equal: >=
+# Less or Equal:    <=
+# Object Identity:  is
+
+
+# False Values:
+    # False
+    # None
+    # Zero of any numeric type
+    # Any empty sequence. For example, '', (), [].
+    # Any empty mapping. For example, {}.
